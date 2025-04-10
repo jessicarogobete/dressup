1773a0434cb9eebd33608734cb94ed0d91faf716
jessicarogobete <jessicarogobete@gmail.com>
2025-04-10 17:03:28 -0400

    add assets

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import rightArrow from './assets/arrow-right.svg'
+import partygirl from './assets/partygirl.svg'
 import './App.css'
 
 function App() {
@@ -8,15 +9,17 @@ function App() {
 
   return (
     <>
-      <div>
+      <h1>Dress Up Party Girl!</h1>
+      <div className='main_area'>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <img src={partygirl} />
+        <button>
+          <img src={rightArrow} />
+        </button>
       </div>
-      <h1>Vite + React</h1>
+
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
