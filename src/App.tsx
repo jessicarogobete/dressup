a9b5435fc0050c9d090f15b782191aab4befb16a
jessicarogobete <jessicarogobete@gmail.com>
2025-04-28 11:32:20 -0400

    main display flex row

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,19 @@
-import { useState } from 'react'
-import viteLogo from '/vite.svg'
 import rightArrow from './assets/arrow-right.svg'
 import partygirl from './assets/Partygirlsmall.png'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
+    <div className='changeroom'>
       <h1>Dress Up Party Girl!</h1>
       <div className='main_area'>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <img src={partygirl} />
+        <img className='partygirl' src={partygirl} />
         <button>
           <img src={rightArrow} />
         </button>
       </div>
-
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </div>
   )
 }
 
