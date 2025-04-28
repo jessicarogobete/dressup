6aa16eb92d14f082fb057a5d091d3f6243d62273
jessicarogobete <jessicarogobete@gmail.com>
2025-04-28 15:19:04 -0400

    main page element positioning

--- src/Components/clothing-buttons.tsx
+++ src/Components/clothing-buttons.tsx
@@ -1,7 +1,11 @@
+import './clothing-buttons.css';
+
 export default function ClothingButtons() {
     const clothingTypes = ['hair', 'tops', 'bottoms', 'shoes'];
     const buttons = clothingTypes.map(item => <button>{item}</button>)
     return (
-        buttons
+        <div className='ButtonsContainer'>
+            {buttons}
+        </div>
     )
 }
