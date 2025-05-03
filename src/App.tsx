67d52e09eb3a425cbca8d95ea8681f6724429c20
jessicarogobete <jessicarogobete@gmail.com>
2025-05-03 19:21:49 -0400

    more styling options

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,18 @@ import pigtails from './assets/Pigtails.png';
 import placeholder from './assets/Placeholder.png';
 import ponytail from './assets/Ponytail.png';
 import fairy_sleeves from './assets/Fix long sleeve fairy top.png';
+import trainers from './assets/Trainers.png';
+import mjs from './assets/Pink Mjs.png';
+import blue_shoes from './assets/Blue shoes.png';
+import ballet_shoes from './assets/Ballet shoes.png';
+import baggy_jeans from './assets/Baggy jeans.png';
+import white_skirt from './assets/White long skirt.png';
+import black_tee from './assets/Plain black tee.png';
+import white_tee from './assets/Plain white tee.png';
+import Button_Bottoms from './assets/Button bottoms.png';
+import Button_Tops from './assets/Button tops.png';
+import Button_Hair from './assets/Button hair.png';
+import Button_Shoes from './assets/Button shoes.png';
 import { useState } from 'react'
 import './App.css'
 import ClothingType from './Components/clothing-type';
@@ -30,15 +42,32 @@ const ClothingTypesMap = new Map<string, CLOTHING_TYPES>([
 ]);
 
 //CONSTANTS
-const shirts = [placeholder, pink_button_shirt, graphic_pink_tee, fairy_sleeves]
+const shirts = [placeholder, pink_button_shirt, graphic_pink_tee, fairy_sleeves, white_tee, black_tee]
 const hairs = [pigtails, pink_bob, ponytail];
-const bottoms = [placeholder, orange_pants];
-
+const bottoms = [placeholder, orange_pants, white_skirt, baggy_jeans];
+const shoes = [placeholder, mjs, blue_shoes, trainers, ballet_shoes];
+function getImage(item) {
+  switch (item) {
+    case 'shoes':
+      return Button_Shoes;
+    case 'hair':
+      return Button_Hair;
+    case 'tops':
+      return Button_Tops;
+    case 'bottoms':
+      return Button_Bottoms
+    default: return ''
+  }
+}
 
 function App() {
   const [state, setState] = useState(CLOTHING_TYPES.NONE);
   let isStart = state === CLOTHING_TYPES.NONE;
-  const buttons = ClothingTypes.map(item => <button onClick={() => setState(ClothingTypesMap.get(item) ?? CLOTHING_TYPES.NONE)}>{item}</button>);
+  const buttons = ClothingTypes.map(item => <img src={
+    getImage(item)
+
+  } onClick={() => setState(ClothingTypesMap.get(item) ?? CLOTHING_TYPES.NONE)
+  }></img >);
 
 
 
@@ -57,8 +86,11 @@ function App() {
         <div className='partygirl'>
           <img className='partygirl-base' src={partygirl} />
           <ClothingType options={hairs} state={state} type={CLOTHING_TYPES.HAIR} />
-          <ClothingType options={shirts} state={state} type={CLOTHING_TYPES.TOP} />
+
+          <ClothingType options={shoes} state={state} type={CLOTHING_TYPES.SHOES} />
           <ClothingType options={bottoms} state={state} type={CLOTHING_TYPES.BOTTOM} />
+          <ClothingType options={shirts} state={state} type={CLOTHING_TYPES.TOP} />
+
         </div>
 
 
@@ -68,4 +100,4 @@ function App() {
   )
 }
 
-export default App
+export default App;
