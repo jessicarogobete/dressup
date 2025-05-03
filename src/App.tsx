8c7d04b121ee1a61e52cad5590286a725d4d4adb
jessicarogobete <jessicarogobete@gmail.com>
2025-05-03 17:52:58 -0400

    add more clothing options

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,46 @@
 import partygirl from './assets/Partygirlsmall.png'
 import pink_button_shirt from './assets/pink_button_shirt.png'
+import graphic_pink_tee from './assets/Graphic pink tee.png';
+import pink_bob from './assets/Pink bob.png';
+import orange_pants from './assets/Orange-pants.png';
+import pigtails from './assets/Pigtails.png';
 import placeholder from './assets/Placeholder.png';
+import ponytail from './assets/Ponytail.png';
+import fairy_sleeves from './assets/Fix long sleeve fairy top.png';
 import { useState } from 'react'
 import './App.css'
 import ClothingType from './Components/clothing-type';
 
+export enum CLOTHING_TYPES {
+  HAIR,
+  TOP,
+  BOTTOM,
+  SHOES,
+  NONE
+}
 
+const ClothingTypes = ["hair", "tops", "bottoms", "shoes"];
+
+
+const ClothingTypesMap = new Map<string, CLOTHING_TYPES>([
+  ["hair", CLOTHING_TYPES.HAIR],
+  ["tops", CLOTHING_TYPES.TOP],
+  ["bottoms", CLOTHING_TYPES.BOTTOM],
+  ["shoes", CLOTHING_TYPES.SHOES]
+]);
 
 //CONSTANTS
-const shirts = [placeholder, pink_button_shirt]
+const shirts = [placeholder, pink_button_shirt, graphic_pink_tee, fairy_sleeves]
+const hairs = [pigtails, pink_bob, ponytail];
+const bottoms = [placeholder, orange_pants];
+
 
 function App() {
-  const [state, setState] = useState('');
-  let isStart = state === '';
-  const clothingTypes = ['hair', 'tops', 'bottoms', 'shoes'];
-  const buttons = clothingTypes.map(item => <button onClick={() => setState(item)}>{item}</button>);
+  const [state, setState] = useState(CLOTHING_TYPES.NONE);
+  let isStart = state === CLOTHING_TYPES.NONE;
+  const buttons = ClothingTypes.map(item => <button onClick={() => setState(ClothingTypesMap.get(item) ?? CLOTHING_TYPES.NONE)}>{item}</button>);
+
+
 
   return (
     <div className='changeroom'>
@@ -26,12 +52,13 @@ function App() {
             <div className='ButtonsContainer'>
               {buttons}
             </div>
-          ) : <button className='exit-button' onClick={() => setState('')}>exit</button>
+          ) : <button className='exit-button' onClick={() => setState(CLOTHING_TYPES.NONE)}>exit</button>
         }
         <div className='partygirl'>
           <img className='partygirl-base' src={partygirl} />
-          <ClothingType options={shirts} state={state}></ClothingType>
-
+          <ClothingType options={hairs} state={state} type={CLOTHING_TYPES.HAIR} />
+          <ClothingType options={shirts} state={state} type={CLOTHING_TYPES.TOP} />
+          <ClothingType options={bottoms} state={state} type={CLOTHING_TYPES.BOTTOM} />
         </div>
 
 
--- src/Components/clothing-type.tsx
+++ src/Components/clothing-type.tsx
@@ -1,11 +1,12 @@
 import { useState } from "react";
 import './clothing-type.css'
 import '../App.css'
+import { CLOTHING_TYPES } from "../App";
 
 
 // stores clothing type options and renders which is currently selected
 // type should be passed in to styles to know where to render the arrow buttons
-export default function ClothingType(props: { options: string[], state: string }) {
+export default function ClothingType(props: { options: string[], state: CLOTHING_TYPES, type: CLOTHING_TYPES }) {
     const [currentOption, setCurrentOption] = useState(0)
 
     //toggle backwards through clothing items
@@ -25,8 +26,10 @@ export default function ClothingType(props: { options: string[], state: string }
             setCurrentOption(0);
         }
     }
-    const showButtons = props.state !== '';
+    const showButtons = props.state == props.type;
 
+
+    //TODO: MAKE THIS A BUTTON CLASS AND REMOVE CLOTHING IMAGE FROM HERE
     return (
         <>
             <img className='partygirl-base' src={props.options[currentOption]}></img>
