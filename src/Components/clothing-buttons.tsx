0bf166a59537c8100f67a7af67e763349838df08
jessicarogobete <jessicarogobete@gmail.com>
2025-05-06 10:59:04 -0400

    add firebase

--- src/Components/clothing-buttons.tsx
+++
@@ -1,9 +0,0 @@
-import './clothing-buttons.css';
-
-// changes state of app to SWITCH_CLOTHING_X of whichever type is passed in
-export default function ClothingButtons(setState: (s: string) => void) {
-
-    return (<></>
-
-    )
-}
--- src/Components/clothing-types.test.ts
+++ src/Components/clothing-types.test.ts
@@ -1,11 +1,14 @@
 import { screen, render } from "@testing-library/react";
 import ClothingType from "./clothing-type";
+import { CLOTHING_TYPES } from "../App";
 
 const testClothes = ['', 'shirt-1']
 
 it('should show toggle forward button', () => {
     render(ClothingType({
-        options: testClothes
+        options: testClothes,
+        state: CLOTHING_TYPES.TOP,
+        type: CLOTHING_TYPES.TOP
     }))
     expect(screen.getByTestId('back-button')).toBeInTheDocument();
 });
